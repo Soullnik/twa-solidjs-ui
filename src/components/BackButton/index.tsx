56f947b58b44ce2f63af07550574bc9c7fae6385
Mikalai Lazitski <mlazitski@elinext.com>
2024-01-12 18:25:46 +0300

    fix: back button

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -1,35 +1,75 @@
-import { useNavigator } from '../../providers'
-import { Component, onCleanup, onMount } from 'solid-js'
-import { useBackButton } from '../../hooks'
+import { RGB, classNames } from '@tma.js/sdk'
+import { useHapticFeedback, useMainButton } from '../../hooks'
+import { Component, Show, createEffect, createSignal, onCleanup } from 'solid-js'
 import styles from './styles.module.scss'
 
-type BackButtonProps = {
+type MainButtonProps = {
+  color?: RGB
+  text: string
   onClick: VoidFunction
+  loading?: boolean
 }
 
-export const BackButton: Component<BackButtonProps> = props => {
-  const backButton = useBackButton()
-  const navigator = useNavigator()
+export const MainButton: Component<MainButtonProps> = props => {
+  const mainButton = useMainButton()
+  const hapticFeedback = useHapticFeedback()
+  const [_] = createSignal(props.loading)
 
-  const onClick = () => {
-    setTimeout(() => {
-      props.onClick()
-    }, 0)
+  const onClickHandler = () => {
+    hapticFeedback().impactOccurred('medium')
+    props.onClick()
   }
 
-  onMount(() => {
-    backButton().on('click', onClick)
-    if (navigator) {
-      navigator.detach()
+  createEffect(() => {
+    if (mainButton) {
+      mainButton().setText(props.text)
+    }
+  })
+
+  createEffect(() => {
+    if (mainButton) {
+      mainButton().setText(props.text)
+    }
+  })
+
+  createEffect(() => {
+    if (props.color && mainButton) {
+      mainButton().setBackgroundColor(props.color)
+    }
+  })
+
+  createEffect(value => {
+    if (props.onClick !== value && mainButton) {
+      mainButton().off('click', onClickHandler)
+      mainButton().on('click', onClickHandler)
+      if (!mainButton().isVisible) {
+        mainButton().show()
+      }
+    }
+  })
+
+  createEffect(() => {
+    if (mainButton) {
+      if (props.loading) {
+        mainButton().disable().showLoader()
+      } else {
+        mainButton().enable().hideLoader()
+      }
     }
   })
 
   onCleanup(() => {
-    backButton().off('click', onClick)
-    if (navigator) {
-      navigator.attach()
+    if (mainButton) {
+      mainButton().off('click', onClickHandler)
+      mainButton().hide()
     }
   })
 
-  return <></>
+  return (
+    <Show when={!mainButton}>
+      <button onClick={props.onClick} class={classNames(styles.mainButton)}>
+        {props.text}
+      </button>
+    </Show>
+  )
 }
